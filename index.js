12135effc29c3be2b0301256edd22a0dd341327f
Azamat-Santa <80563408+Azamat-Santa@users.noreply.github.com>
2022-01-17 00:05:09 +0600

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let FeaturesCardTexts = document.querySelectorAll('.FeaturesCardText')
+// FeaturesCardTexts=Array.from(FeaturesCardTexts)
+// console.log(Array.isArray(FeaturesCardTexts));
+// for (let i = 0; i < FeaturesCardTexts.length; i++) {
+//     const element = FeaturesCardTexts[i];
+//     console.log(element);
+    
+// }
+// FeaturesCardTexts.map(el=>console.log(el.clientHeight))
+// console.log(FeaturesCardTexts);
